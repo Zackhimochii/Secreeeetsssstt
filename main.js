b062644c31912691bb980859cd8fd1f64d0e3aaa
Zackhimochii <44330821+Zackhimochii@users.noreply.github.com>
2024-11-24 17:42:22 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 onload = () =>{
         document.body.classList.remove("container");
+        
+window.onload = function() {
+  var audio = document.getElementById("background-music");
+  audio.play();  // Play the audio when the page loads
 };
